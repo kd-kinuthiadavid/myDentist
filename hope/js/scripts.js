eea19a22235969648bd24c71a04ae38017c052c7
Muriithi-003 <vionnamuriithi@gmail.com>
2018-05-15 17:33:57 +0300

    hope hospital development

---
+++ hope/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".dentistselfies").hide();
+  $(".dentistH").click(function(){
+    $(".dentistselfies").toggle(1000);
+  });
+});
+$(document).ready(function() {
+  $(".Customers").hide();
+  $(".meno").click(function(){
+    $(".Customers").toggle(2000);
+  });
+});
