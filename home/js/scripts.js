c1a3b0ddbca0cf5bacf5e94bb24a53c528786354
JohnPaul Gitata <johnpaulgitata@gmail.com>
2018-05-16 08:56:44 +0300

    merging

--- home/js/scripts.js
+++ home/js/scripts.js
@@ -7,6 +7,7 @@ function carousel() {
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";
     }
+    
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}
     x[myIndex-1].style.display = "block";
