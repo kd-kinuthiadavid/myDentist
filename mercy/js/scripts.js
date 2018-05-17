7d7ff32681a0cc38120261263c9f848cb8a93f86
Muriithi-003 <vionnamuriithi@gmail.com>
2018-05-17 08:51:15 +0300

    mercy dentistry

---
+++ mercy/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".doctors").hide();
+  $(".dentistH").click(function(){
+    $(".doctors").toggle(1000);
+  });
+});
+$(document).ready(function() {
+  $(".Customers").hide();
+  $(".meno").click(function(){
+    $(".Customers").toggle(1000);
+  });
+});
